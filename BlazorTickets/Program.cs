2979af44752d1c4b5cf5403fe8b52ca4a5671efe
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-03-18 00:26:46 -0600

    Added services

--- BlazorTickets/Program.cs
+++ BlazorTickets/Program.cs
@@ -1,9 +1,15 @@
 ﻿using BlazorTickets.Components;
 using BlazorTickets.Data;
 using BlazorTickets.Services;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using SixLabors.ImageSharp;
 using TicketLibrary.Services;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,23 +24,71 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<MailMailMail>();
 builder.Services.AddDbContext<PostgresContext>(options => options.UseNpgsql(builder.Configuration["Postgres"]));
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
+
+builder.Logging.ClearProviders();
+
+const string serviceName = "carlos service";
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource =>
+          resource.AddService(serviceName))
+    .WithTracing(b =>
+    {
+        b
+        .AddAspNetCoreInstrumentation()
+        .AddSource(CarlosTracing.traceName)
+        .AddSource(CarlosTracing.traceName2)
+        .AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri("http://otel-collector:4317/");
+        });
+    })
+    .WithMetrics(b => 
+    {
+        b
+        .AddAspNetCoreInstrumentation()
+        .AddMeter(CarlosMetric.MetricName)
+        .AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri("http://otel-collector:4317/");
+        });
+    });
+    
+  
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+//Tracing Activity endpoint
+
+app.MapGet("/carlosCustomTrace1", () => CarlosTracing.MyActivitySource.StartActivity("CarlosActivty1"));
+app.MapGet("/carlosCustomTrace2", () => CarlosTracing.MyActivitySource2.StartActivity("CarlosActivty2"));
+
+
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                }
+});
 
 app.MapControllers();
 app.UseStaticFiles();
@@ -46,4 +100,4 @@ app.MapRazorComponents<App>()
 
 app.Run();
 
-public partial class Program { };
+public partial class Program {  };
